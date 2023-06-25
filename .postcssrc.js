30bf6ece756a6195d68ded9d80a124413deaf50b
SgQyu <winer123456@gmail.com>
2023-06-25 16:47:38 +0900

    Create Parcel

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,5 +1,28 @@
+// ESM(ECMA Script Module) : 자바스크립트의 가져오기 내보내기 방식, 브라우저에서 사용되서 동작,
+
+// import autoprefixer from 'autoprefixer'
+
+// export {
+//   plugins: [
+//     autoprefixer
+//   ]
+// }
+
+// CommonJS : Node JS 환경에서의 가져오기 내보내기 방식, 번들러를 이용해서 변환하는 용도
+
+// 1> defalult
+const autoprefixer = require('autoprefixer')
+
 module.exports = {
   plugins: [
-    require('autoprefixer')
+    autoprefixer
   ]
 }
+
+// 2> 간소화
+
+// module.exports = {
+//   plugins: [
+//     require('autoprefixer')
+//   ]
+// }
